92bb2041d86b6eac937a54789b5abd8ec1f2e1c1
dacanizares <dacanizares@users.noreply.github.com>
2022-11-13 21:08:02 +0000

    deploy: 7aa0d97d38f28bbaedeb2c1751273b6439c576f9

--- index.min.05c59283f1ddd348b0c010d54e942f1164bc4955c07e583174c20c4800ec048c017e674b9f4d49ba15f144ba07f0fa9838f4670d23bf4bf89ccece5514c49f86.js
+++ index.min.05c59283f1ddd348b0c010d54e942f1164bc4955c07e583174c20c4800ec048c017e674b9f4d49ba15f144ba07f0fa9838f4670d23bf4bf89ccece5514c49f86.js
@@ -22,7 +22,7 @@ sudo dnf install ffmpegthumbs Optimize startup by masking systemd-udev-settle:
 sudo systemctl mask systemd-udev-settle More info
 Optimize startup by disabling NetworkManager-wait-online.service:
 sudo systemctl disable NetworkManager-wait-online.service More info
-Disable check for update at startup (KDE):
+Disable check for updates at startup (KDE):
 sudo mv /etc/xdg/autostart/org.kde.discover.notifier.desktop /etc/xdg/autostart.disabled/org.kde.discover.notifier.desktop Software #Git #Install instructions Terminal #Install zsh Run command: zsh Install and run Powerlevel10k Set it as your default shell: usermod --shell /usr/bin/zsh root usermod --shell /usr/bin/zsh YOUR_USERNAME To re-run the setup:
 p9k_configure VS Code (VSCodium) #Install VSCodium
 Set VSCode/Codium as default editor. more info
