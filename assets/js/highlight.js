5a62092f4614a7ae7e3dde0123a01c0992b563f0
dacanizares <dacanizares@outlook.com>
2024-06-06 16:42:32 -0500

    Added python
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
    Co-Authored-By: Edwin Osorio <30908546+edwin1106@users.noreply.github.com>

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js/lib/core';
 
 import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
 import ini from 'highlight.js/lib/languages/ini';
@@ -12,6 +13,9 @@ import csharp from 'highlight.js/lib/languages/csharp';
 
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('js', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('py', python);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('sh', bash);
